42f07ee4659bce629fd185a15f40fdba56f9d802
Harits <mharitsnf@gmail.com>
2024-06-20 18:07:18 +0200

    Add sundial rotation

--- assets/prefabs/sundial_manager/sundial_manager.gd
+++ assets/prefabs/sundial_manager/sundial_manager.gd
@@ -1,5 +1,7 @@
 class_name SundialManager extends Node3D
 
+@export_group("Parameters")
+@export var latmes_rotate_speed: float = 5.
 @export_group("References")
 @export var sundial_center: Marker3D
 @export_subgroup("Packed scenes")
@@ -7,10 +9,13 @@ class_name SundialManager extends Node3D
 @export var sundial_pscn: PackedScene
 
 var latitude_measure: Node3D
+var latitude_measure_mesh: MeshInstance3D
 var sundial: Node3D
 
 var latitude_measure_rt: RemoteTransform3D
 var sundial_rt: RemoteTransform3D
+
+const SUNDIAL_ROTATION_STEP: float = deg_to_rad(15)
 
 func _ready() -> void:
     assert(sundial_center)
@@ -21,6 +26,8 @@ func _ready() -> void:
 
 func player_input_process(_delta: float) -> void:
     _get_exit_sundial_input()
+    _get_rotate_sundial_input()
+    _get_rotate_latmes_input()
 
 func _get_exit_sundial_input() -> void:
     if Input.is_action_just_pressed("toggle_boat_sundial"):
@@ -28,6 +35,24 @@ func _get_exit_sundial_input() -> void:
 
         var boat_pd: PlayerActorManager.PlayerData = State.game_pam.get_player_data(PlayerActorManager.PlayerActors.BOAT)
         State.game_pam.change_player_data(boat_pd)
+
+func _get_rotate_latmes_input() -> void:
+    var amount: float = Input.get_axis("rotate_latmes_left", "rotate_latmes_right")
+    _rotate_latmes(amount)
+
+func _get_rotate_sundial_input() -> void:
+    if Input.is_action_just_pressed("rotate_sundial_left"):
+        _rotate_sundial(-SUNDIAL_ROTATION_STEP)
+    if Input.is_action_just_pressed("rotate_sundial_right"):
+        _rotate_sundial(SUNDIAL_ROTATION_STEP)
+
+func _rotate_latmes(amount: float) -> void:
+    if amount != 0:
+        latitude_measure_mesh.rotate_y(amount * latmes_rotate_speed * get_process_delta_time())
+
+func _rotate_sundial(amount: float) -> void:
+    if amount != 0:
+        sundial.rotate_y(amount)
 
 ## Private. Initiate sundial using remote transform.
 func _initiate_sundial() -> void:
@@ -39,6 +64,9 @@ func _initiate_sundial() -> void:
     level.add_child.call_deferred(latitude_measure)
     
     await latitude_measure.ready
+
+    latitude_measure_mesh = latitude_measure.get_node("LatitudeMeasure")
+    assert(latitude_measure_mesh)
 
     var rtb_sundial: Common.RemoteTransform3DBuilder = Common.RemoteTransform3DBuilder.new()
     rtb_sundial.rename("Sundial")
