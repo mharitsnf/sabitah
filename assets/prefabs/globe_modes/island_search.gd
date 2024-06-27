8abe2dd2ba173197d5d1c8f13b02c0782cf0a89c
Harits <mharitsnf@gmail.com>
2024-06-27 16:10:44 +0200

    Implement island search

--- assets/prefabs/globe_modes/island_search.gd
+++ assets/prefabs/globe_modes/island_search.gd
@@ -5,5 +5,12 @@ class_name IslandSearch extends LatLongSearch
 
 func _ready() -> void:
 	super()
-
 	assert(level_anim)
+
+func player_input_process(delta: float) -> void:
+	super(delta)
+	_get_select_location_input()
+
+func _get_select_location_input() -> void:
+	if Input.is_action_just_pressed("select_location_on_globe"):
+		print("location: ", query_res)
