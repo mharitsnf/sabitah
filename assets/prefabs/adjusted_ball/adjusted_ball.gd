183e1924e408a32a189e68aab9c1a1017dfd778f
Harits <mharitsnf@gmail.com>
2024-07-17 20:12:32 +0200

    Restructure prefab folder

--- assets/prefabs/adjusted_ball/adjusted_ball.gd
+++
@@ -1,5 +0,0 @@
-extends MeshInstance3D
-
-func _process(_delta: float) -> void:
-    var new_quat: Quaternion = Common.Geometry.recalculate_quaternion(basis, global_position.normalized())
-    quaternion = new_quat
